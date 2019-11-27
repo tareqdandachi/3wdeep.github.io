e6eb8d5d7c3d0cd4f8341cf86678e3a507c1e19b
Tareq El Dandachi <tareqdandachi@gmail.com>
2019-11-26 21:36:03 -0500

    Finished purity test code

--- assets/purity_test/purity_test.js
+++ assets/purity_test/purity_test.js
@@ -1,5 +1,97 @@
+window.onload = function() {
+
+ fill_in_questions();
+
+ reset_display();
+
+}
+
+function reset_display() {
+
+  document.getElementById("purity_form").style.visibility = 'visible';
+
+  document.getElementById("purity_form").style.display = 'block';
+
+  document.getElementById("retake_button").style.visibility = 'hidden';
+
+  document.getElementById("score_text").style.visibility = 'hidden';
+
+}
+
 function fill_in_questions() {
 
+  question_bucket = document.getElementById("purity_form");
 
+  questions = document.getElementById("questions").textContent;
+
+  questions = questions.split("\n");
+
+  html_content = "";
+
+  for (var i=0; i < questions.length; i++) {
+
+    if (questions[i].replace(/ /g, '') != "") {
+
+      html_content += '<p><input type="checkbox" id="checkbox-' + i + '"> ' + String(i+1) + ". " + questions[i] + '</p>';
+
+    }
+
+  }
+
+  question_bucket.innerHTML = html_content + '<a onclick="calculate_score()">Calculate Score</a>';
+
+}
+
+function calculate_score() {
+
+  questions = document.getElementById("questions").textContent;
+
+  question_length = questions.split("\n").length;
+
+  score = 100;
+
+  for (var i=0; i < question_length; i++) {
+
+    if (questions[i].replace(/ /g, '') != "") {
+
+      if (get_purity_of_index(i)) {
+
+        score -= 1;
+
+      }
+
+    }
+
+  }
+
+  display_score(score);
+
+}
+
+function get_purity_of_index(i) {
+
+  element = document.getElementById("checkbox-"+i);
+
+  if (element) {
+
+    return element.checked;
+
+  }
+
+  return false;
+
+}
+
+function display_score(score) {
+
+  document.getElementById("your_score").innerHTML = score;
+
+  document.getElementById("score_text").style.visibility = 'visible';
+
+  document.getElementById("purity_form").style.visibility = 'hidden';
+
+  document.getElementById("purity_form").style.display = 'none';
+
+  document.getElementById("retake_button").style.visibility = 'visible';
 
 }
--- purity_test.md
+++ purity_test.md
@@ -4,9 +4,16 @@ title: deep purity test
 permalink: /purity_test
 permalink_name: /purity-test
 
-detail_image: assets/logo_corrupt.png
+detail_image: assets/purity_test/purity.png
+js_file: assets/purity_test/purity_test.js
 ---
 
 # Rice Purity Test for The Deep
 
-<p id="questions" hidden=True onload="fill_in_questions()">{% include_relative /assets/purity_questions.txt %}</p>
+<p id="questions" hidden>{% include_relative /assets/purity_test/purity_questions.txt %}</p>
+
+<h1 id="score_text">Your Purity Score is <strong class="invert" id="your_score">100</strong></h1>
+
+<form id="purity_form"></form>
+
+<a id="retake_button" onclick="reset_display()">Reset</a>
